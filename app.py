0a3a4a6f743ee191664c185b507442826cafe164
2025ab05031 <2025ab05031@wilp.bits-pilani.ac.in>
2026-01-21 10:44:07 +0530

    Update app.py
    sns removed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, 
     f1_score, roc_auc_score, matthews_corrcoef, confusion_matrix
